bdef3a3246b046dd263b5592d25c388baa6245cf
claudiotruong1 <claudiorasmussen@gmail.com>
2021-11-10 21:36:16 +0100

    creating the login page
    Her laver vi et login side, der tjekker om brugerens input stemmer overens med array'et.

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-var hej = "hej"
-console.log(hej);
+var objPeople = [
+    {
+        username: "Claudio",
+        password: "Truong"
+    },
+    {
+        username: "Mikkel",
+        password: "Mikkelsen"
+    },
+    {
+        username: "Knud",
+        password: "Knudsen"
+    }
+]
+
+
+function getInfo() {
+    var username = document.getElementById("username").value
+    var password = document.getElementById("password").value
+   
+    for (i = 0; i < objPeople.length; i++) {
+        if(username == objPeople[i].username && password == objPeople[i].password) {
+            console.log(username + " is logged in!!!")
+            return
+        }
+    }
+    console.log("incorrect username or password")
+}
