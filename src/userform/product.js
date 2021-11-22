1e0894a0335918fd0632cad4bcfddd1b8f7b742b
claudiotruong1 <claudiorasmussen@gmail.com>
2021-11-22 14:28:36 +0100

    Creating the product form

---
+++ src/userform/product.js
@@ -0,0 +1,10 @@
+class products {
+    constructor(product, price, category) {
+      this.product = product;
+      this.price = price;
+      this.category = category;
+    }
+  }
+  
+  module.exports = products;
+  
