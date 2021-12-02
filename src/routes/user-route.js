e033c965771f2580fd067d5681c6e230d0cb009a
claudiotruong1 <claudiorasmussen@gmail.com>
2021-12-02 21:49:36 +0100

    Trying to update the user

--- src/routes/user-route.js
+++ src/routes/user-route.js
@@ -20,7 +20,7 @@ router.post("/login", (req, res) => {
   const found = db.findUser(user);
   if (found) {
     if (user.password == found.password) {
-      res.status(200).send(true);
+      res.status(200).send(true); // send req.body.id tilbage
     } else {
       res.status(401).send(false);
     }
@@ -28,5 +28,12 @@ router.post("/login", (req, res) => {
     res.status(404).send(false);
   }
 });
+
+router.post("/update", (req, res) => {
+  const user = new userModel(req.body.id, req.body.email, req.body.password);
+  db.updateUser(user);
+  res.status(200).send(true);
+});
+
 
 module.exports = router;
