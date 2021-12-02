e80fc5b1511822a1f907f7068012363f852c8610
claudiotruong1 <claudiorasmussen@gmail.com>
2021-12-02 21:49:08 +0100

    Creating the show products route

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const productModel = require("./../userform/product");
 const db = require("./../database/productDB");
 const { updateProduct } = require("./../database/productDB");
+const { appendFile } = require("fs");
 // const fs = require('fs')
 
 
@@ -15,25 +16,29 @@ router.post("/create", (req, res) => {
 });
 
 router.delete("/delete", (req, res) => {
-    //const products = new productModel(req.body.product, req.body.price, req.body.category, req.body.picture);
     db.deleteProduct(products);
     res.status(200).send(true);
 });
 
-router.get("/update", (req, res) => {
-  // const qstring = url.parse(req.url, true).query;
+router.post("/update", (req, res) => {
    const product = new productModel(req.body.id, req.body.product, req.body.price, req.body.category, req.body.picture);
-
- //  db.updateProduct(product);
+   console.log(req.body.category)
+    db.updateProduct(product);
    res.status(200).send(true)
 });
 
 // jeg tilføjer blot kategori til stien - fjern /:category hvis det ikke fungerer.
 
 router.get("/getAllProducts", (req, res) => {
-   //  const {category} = req.params;
     const allProducts = db.getProducts()
     res.status(200).send(allProducts);
-})
+});
+
+router.get("/showProducts/:category", (req, res) => {
+    const { category } = req.params;
+    const allMatching = db.getSpecificProducts(category)
+
+    res.status(200).send(allMatching)
+});
     
 module.exports = router;
