a3247d7603da61d597581f0be5f8dbde775901ba
claudiotruong1 <claudiorasmussen@gmail.com>
2021-12-04 20:39:35 +0100

    Formatting

--- src/views/updateProduct.js
+++ src/views/updateProduct.js
@@ -1,53 +1,48 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const urlParams = new URLSearchParams(window.location.search);
-  
-  const idLoaded = urlParams.get("id");
-  
-  //const product = document.getElementById("product");
-  //const price = document.getElementById("price");
-  //const category = document.getElementById("category");
+  const urlParams = new URLSearchParams(window.location.search); // vi laver en variabel, som indeholder de parsede søgeparametre
 
-  const productLoaded = urlParams.get("product");
+  const idLoaded = urlParams.get("id");
+
+  const productLoaded = urlParams.get("product"); // vi laver en variabel, der returnerer den første værdi, der er knyttet til produkter
   document.getElementById("product").value = productLoaded;
- 
+
   const priceLoaded = urlParams.get("price");
   document.getElementById("price").value = priceLoaded;
- 
+
   const categoryLoaded = urlParams.get("category");
   document.getElementById("category").value = categoryLoaded;
 
-// update
-  document.getElementById("update-products").addEventListener("submit", (event) => {
-    event.preventDefault();
+  document
+    .getElementById("update-products")
+    .addEventListener("submit", (event) => {
+      event.preventDefault();
 
-    const product = document.getElementById("product").value;
-    const price = document.getElementById("price").value;
-    const category = document.getElementById("category").value;
-      
-    const payload = {
-      id: idLoaded,
-      product: product,
-      price: price,
-      category: category
-    };
+      const product = document.getElementById("product").value;
+      const price = document.getElementById("price").value;
+      const category = document.getElementById("category").value;
 
-       fetch("http://localhost:7000/products/update", {
-           method: "POST",
-           headers: {
-               "Content-Type": "application/json"
-           },
-           body: JSON.stringify(payload),
-       })
-       //.then((response) => response.json())
-       .then((response) => {
-           if(response) {
-               window.alert("Tillykke! Du har netop opdateret din vare.")
-               location.href = "/addProduct.html";
-           }
-       })
-       .catch(() => {
-           window.alert("Error.")
-       });
+      const payload = {
+        id: idLoaded,
+        product: product,
+        price: price,
+        category: category,
+      };
+
+      fetch("http://localhost:7000/products/update", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      })
+        .then((response) => {
+          if (response) {
+            window.alert("Tillykke! Du har netop opdateret din vare.");
+            location.href = "/addProduct.html";
+          }
+        })
+        .catch(() => {
+          window.alert("Error.");
+        });
     });
- });
- 
+});
