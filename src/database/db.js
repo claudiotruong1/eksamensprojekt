14e35423cd202dc2d9144840e10f0ecc609d7737
claudiotruong1 <claudiorasmussen@gmail.com>
2021-12-04 19:38:43 +0100

    Adding comments

--- src/database/db.js
+++ src/database/db.js
@@ -1,26 +1,31 @@
+// fs sørger for at gemme/læse vores users.json fil til vores computer
 var fs = require("fs");
 const { runInThisContext } = require("vm");
 
 const PATH = __dirname + "/../../data";
 const userFile = "/users.json";
 
+
+// En database der kan gemme vores users
 class dataBase {
   constructor() {
-    this.users = this.openFile(userFile);
+    this.users = this.openFile(userFile); // vi laver en variable, som indeholer vores users.json fil
   }
 
+  // En funktion der gemmer filer 
   saveFile(fileName, contentString) {
-    fs.writeFileSync(PATH + fileName, contentString);
+    fs.writeFileSync(PATH + fileName, contentString); // en metoder der accepterer vores tre parametre
   }
 
+  // En funktion der åbne filer -> vi kan se, hvilke brugere vi har
   openFile(fileName) {
-    const file = fs.readFileSync(PATH + fileName);
-    return JSON.parse(file);
+    const file = fs.readFileSync(PATH + fileName); // vi definerer en variable, som læser vores users.json fil synkront
+    return JSON.parse(file); // vi giver en værdi væk fra funktionen, og herefter parser den
   }
-
+// En funktion der kan gemme en user til en fil
   saveUser(user) {
-    this.users.push(user);
-    this.saveFile(userFile, JSON.stringify(this.users));
+    this.users.push(user); // vi referer til vores variabel users, og herefter "pusher" vi for tilføje et element til vores array i json filen
+    this.saveFile(userFile, JSON.stringify(this.users)); // her sørger vi for, at det som vi har opdateret users med, bliver lagt i filen
   }
 
   deleteUser(user) {
@@ -28,6 +33,7 @@ class dataBase {
     this.saveFile(userFile, JSON.stringify(this.users));
   }
 
+// vi finder vores user ud fra den email, som er registreret i vores database
   findUser(user) {
     return this.users.find((x) => user.email == x.email);
   }
