22beb002c3782bed8773ce6462346bdc0e06a1d9
claudiotruong1 <claudiorasmussen@gmail.com>
2021-12-04 20:39:17 +0100

    Formatting

--- src/views/index.js
+++ src/views/index.js
@@ -1,42 +1,43 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    const user = localStorage.getItem("user");
-    if (!user) { // hvis brugeren ikke findes
-      location.href = "/login.html"; // bliver man sendt tilbage til login html siden, da vedkommende ikke er logget ind
-    }
+  const user = localStorage.getItem("user");
+  if (!user) {
+    // hvis brugeren ikke findes
+    location.href = "/login.html"; // bliver man sendt tilbage til login html siden, da vedkommende ikke er logget ind
+  }
   // vi finder vores "delete" form, og herefter for xx
-    document.getElementById("delete").addEventListener("submit", (event) => { // vi laver en arrow function
-      event.preventDefault(); // xx
-  
-      const user = JSON.parse(localStorage.getItem("user")); // vi henter den user, som er logget ind, og herefter laver vi det om til et json objekt for at vi kan håndtere det i JavaScript
-  
-      fetch("http://localhost:7000/users/delete", { // vi kalder på vores endpoint fra vores user-route.js fil 
-        method: "DELETE", // vi angiver vores metode
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(user),
+  document.getElementById("delete").addEventListener("submit", (event) => {
+    // vi laver en arrow function
+    event.preventDefault(); // xx
+
+    const user = JSON.parse(localStorage.getItem("user")); // vi henter den user, som er logget ind, og herefter laver vi det om til et json objekt for at vi kan håndtere det i JavaScript
+
+    fetch("http://localhost:7000/users/delete", {
+      // vi kalder på vores endpoint fra vores user-route.js fil
+      method: "DELETE", // vi angiver vores metode
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        if (response) {
+          localStorage.removeItem("user"); // hvis vores repsonse lykkedes sletter vi brugeren fra localstorage
+          location.href = "/login.html";
+        }
       })
-        .then((response) => response.json())
-        .then((response) => {
-          if (response) {
-            localStorage.removeItem("user"); // hvis vores repsonse lykkedes sletter vi brugeren fra localstorage
-            location.href = "/login.html";
-          }
-        })
-        .catch(() => {
-          window.alert("Noget gik galt.");
-        });
-    });
+      .catch(() => {
+        window.alert("Noget gik galt.");
+      });
+  });
 
-    document.getElementById("logoutUser").addEventListener("submit", (event) => {
-        event.preventDefault();
+  document.getElementById("logoutUser").addEventListener("submit", (event) => {
+    event.preventDefault();
 
-
-    if(user) {
-        localStorage.removeItem("user");
-        location.href ="/login.html";
-
-    };
+    if (user) {
+      localStorage.removeItem("user");
+      location.href = "/login.html";
+    }
   });
 
   document.getElementById("opdater").addEventListener("click", (event) => {
@@ -44,10 +45,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
     const urlParams = new URLSearchParams(window.location.search);
     const emailLoaded = urlParams.get("email");
 
-    console.log(emailLoaded)
+    console.log(emailLoaded);
     location.href = `/updateUser.html?email=${emailLoaded}`;
   });
 });
-
-
-
