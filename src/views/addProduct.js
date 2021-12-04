57bdc17ab9218dfdaf10dfd0419f24d0980cdf00
claudiotruong1 <claudiorasmussen@gmail.com>
2021-12-04 20:39:13 +0100

    Formatting

--- src/views/addProduct.js
+++ src/views/addProduct.js
@@ -1,22 +1,22 @@
 // vi anvender addEventListener for at bruge vores input til at gøre noget nyt
 document.addEventListener("DOMContentLoaded", (event) => {
-    
-    // vi kalder på vores URL, og herefter giver den vores ønskede metode, som er "GET"
+  // vi kalder på vores URL, og herefter giver den vores ønskede metode, som er "GET"
   fetch("http://localhost:7000/products/getAllProducts", {
     method: "GET",
     headers: {
-      "Content-Type": "application/json", 
+      "Content-Type": "application/json",
     },
   })
     .then((response) => response.json())
     .then((response) => {
       console.log(response);
-      response.forEach((elem) => { // For-each løkke som lægger indsætter vores produkt variable, og lægger nye variable til så snart den findet et 
+      response.forEach((elem) => {
+        // For-each løkke som lægger indsætter vores produkt variable, og lægger nye variable til så snart den findet et
         var html = "<tr>";
         html += `<td>${elem.product}</td>`;
         html += `<td>${elem.price}</td>`;
         html += `<td>${elem.category}</td>`;
-        html += `<td><img src="${elem.picture}" width="60" height="30"></td>`
+        html += `<td><img src="${elem.picture}" width="60" height="30"></td>`;
         html += `<td><a href="updateProduct.html?id=${elem.id}&product=${elem.product}&price=${elem.price}&category=${elem.category}">Opdater vare</a></td>`;
         html += "</tr>";
         document.getElementById("varer").innerHTML += html;
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
       window.alert("Noget gik galt.");
     });
 
-    // vi tager fat i vores form fra html filen, og herefter reagerer på vores submit
+  // vi tager fat i vores form fra html filen, og herefter reagerer på vores submit
   document.getElementById("products").addEventListener("submit", (event) => {
     event.preventDefault(); // den stopper html siden med at genindlæse
 
@@ -35,14 +35,14 @@ document.addEventListener("DOMContentLoaded", (event) => {
     const category = document.getElementById("category").value;
     const picture = document.getElementById("picture").value;
 
-    var id = Math.floor(Math.random() * 100000) // vi genererer et unikt id med et unikt nummer
-    
+    var id = Math.floor(Math.random() * 100000); // vi genererer et unikt id med et unikt nummer
+
     const payload = {
       id: id.toString(), // gemmer id som en streng
       product: product,
       price: price,
       category: category,
-      picture: './product.png',
+      picture: "./product.png",
     };
 
     fetch("http://localhost:7000/products/create", {
@@ -61,6 +61,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
       })
       .catch(() => {
         window.alert("Noget gik galt.");
-      }); 
+      });
   });
 });
