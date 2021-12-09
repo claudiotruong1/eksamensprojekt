a6ebe6412816e22c8bd0db12c1b2ba608a1ca9b6
claudiotruong1 <claudiorasmussen@gmail.com>
2021-12-09 14:04:01 +0100

    Addding comments

--- src/routes/user-route.js
+++ src/routes/user-route.js
@@ -5,15 +5,15 @@ const db = require("../database/db"); // vi henter vores db fil
 
 // Et endpoint som kan tilføje en bruger
 router.post("/create", (req, res) => {
-  const user = new userModel(req.body.id, req.body.email, req.body.password); //vi laver en user varibel, som indeholder vores user objekt, og herefter tilgår vi hhv. id, email og password ved at bruge req.body.xx
+  const user = new userModel(req.body.id, req.body.email, req.body.password); //vi laver en user variabel, som indeholder vores user objekt, og herefter tilgår vi hhv. id, email og password ved at bruge req.body.xx
   db.saveUser(user); // hver gang vi kører vores post request, bliver vores users gemt
   res.status(200).send(true); // her giver et svar tilbage til brugeren, som sender et request
 });
 
 router.delete("/delete", (req, res) => {
-  // vi laver en route, som befinder sig i en arrow function
+  // vi laver et delete endpoint, som befinder sig i en arrow function
   const user = new userModel(req.body.id, req.body.email, req.body.password);
-  db.deleteUser(user); // en function som blot slette en bruger ud fra email - mere præcist - hvis emailen er den samme slettes brugeren
+  db.deleteUser(user); // en funktion som blot slette en bruger ud fra email - mere præcist - hvis emailen er den samme slettes brugeren
   res.status(200).send(true); // vi returnerer et svar til brugeren om, at deres request lykkedes
 });
 
